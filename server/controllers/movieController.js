976166bc7500882dfd9950fb0c7ccc76aa73e1c8
Kelli Landry <kellilandry@Kellis-MacBook-Pro.local>
2022-07-25 18:12:55 -0500

    intital setup

---
+++ server/controllers/movieController.js
@@ -0,0 +1,11 @@
+require('../models/db');
+
+const Movie = require('../models/movie')
+
+
+//api/movies - get all movies
+exports.listMovies = async(req, res) => {
+
+  res.send('Hello');
+
+}
---
+++ server/models/movie.js
@@ -0,0 +1,22 @@
+const mongoose = require('mongoose');
+
+const movieSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: "This is required"
+  },
+  description: {
+    type: String,
+    required: true
+  },
+  category: {
+    type: Array,
+    required: true
+  },
+  thumbnail: {
+    type: String,
+    required: true
+  }
+})
+
+module.exports = mongoose.model('Movie', movieSchema);
